fd1e92777613b18d5990dd04da62eb38dcbe3477
aureliuszek <48626117+aureliuszek@users.noreply.github.com>
2021-02-14 03:32:21 +0100

    added lab3
    added lab3

--- Wsei.ExchangeThings.Web/Startup.cs
+++ Wsei.ExchangeThings.Web/Startup.cs
@@ -4,10 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Wsei.ExchangeThings.Web.Database;
 using Wsei.ExchangeThings.Web.Filters;
 
 namespace Wsei.ExchangeThings.Web
@@ -21,13 +22,19 @@ namespace Wsei.ExchangeThings.Web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddControllersWithViews(config =>
+            {
+                config.Filters.Add<MyCustomActionFilter>();
+            });
+
+            services.AddTransient<MyCustomActionFilter>();
+            services.AddDbContext<ExchangesDbContext>(options => options
+                .UseSqlServer(Configuration.GetConnectionString("ExchangeThings"))
+            );
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,10 +44,7 @@ namespace Wsei.ExchangeThings.Web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
--- Wsei.ExchangeThings.Web/wwwroot/js/addNewItem.js
+++ Wsei.ExchangeThings.Web/wwwroot/js/addNewItem.js
@@ -1,30 +1,34 @@
-﻿
-
-(function () {
+﻿(function () {
     const alertElement = document.getElementById("success-alert");
-    const formElement = document.forms[0];
+    const errorAlertElement = document.getElementById("error-alert");
+    const msg = document.getElementById("msg");
+    const form = document.forms[0];
     const addNewItem = async () => {
-        const formData = new FormData(document.querySelector('form'))
-
-        const formvals = {}
-        for (const pair of formData.entries()) {
-            formvals[pair[0]] = pair[1];
-        }
-        const requestData = {
+        // 1. read data from the form
+        const requestData = JSON.stringify({ Name: document.getElementById("Name").value, Description: document.getElementById("Description").value, IsVisible: document.getElementById("IsVisible").checked });
+        // 2. call the application server using fetch method
+        const url = "/api/item";
+        const response = await fetch(url, {
             method: 'POST',
-            body: JSON.stringify(formvals)
-        };
-        const response = await fetch('/api/Ajax', requestData);
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: requestData
+        });
         const responseJson = await response.json();
         if (responseJson.success) {
-            alertElement.style.display = 'block';
+            alertElement.style.display = "";
+        } else {
+            errorAlertElement.style.display = "";
+            msg.innerText = responseJson.message;
         }
     };
-    window.addEventListener("DOMContentLoaded", () => {
-        formElement.addEventListener("submit",
-            event => {
-                event.preventDefault();
-                addNewItem().then(() => console.log("added successfully"));
-            });
+    window.addEventListener("load", () => {
+        form.addEventListener("submit", event => {
+            event.preventDefault();
+            alertElement.style.display = "none";
+            errorAlertElement.style.display = "none";
+            addNewItem().then(() => console.log("added successfully"));
+        });
     });
 })();
